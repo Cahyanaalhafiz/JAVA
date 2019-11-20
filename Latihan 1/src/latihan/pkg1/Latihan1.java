1a8af13fee03058b54b0bd2c52a3c5d374309330
Cahyanaalhafiz <cahyanaalhafiz008@gmail.com>
2019-11-20 10:43:15 +0700

    first commit

---
+++ Latihan 1/src/latihan/pkg1/Latihan1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg1;
+
+/**
+ * @author cahyana
+ * NAMA                 : CAHYANA
+ * KELAS                : TI -1B
+ * NIM                  : A2.1900029
+ * Deskripsi Program    : Perintah Menampilkan Value ke Layar
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Cahyana");
+        System.out.println("ALAMAT\t\t: Tanjungsari ");
+        System.out.println("TEMPAT LAHIR\t: Sumedang");
+        System.out.println("TANGGAL LAHIR\t: 28 Agustus 2001");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Main Game");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+    
+}
